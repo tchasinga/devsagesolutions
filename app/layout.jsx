18520506cf72c13862e9de5a11edbfd54452fa65
tchasinga <tchasingajacques@gmail.com>
2025-11-12 08:53:08 +0300

    Update RootLayout to suppress hydration warnings for improved rendering consistency.

--- app/layout.jsx
+++ app/layout.jsx
@@ -57,7 +57,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${monaSans.variable} antialiased`}
       >
